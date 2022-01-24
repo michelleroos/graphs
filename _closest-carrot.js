84cd1353edc7983591be425ad6fb0e50b65e5327
michelleroos <hello.michelleroos@gmail.com>
2022-01-24 12:04:14 -0500

    closest carrot

---
+++ _closest-carrot.js
@@ -0,0 +1,102 @@
+/*
+closest carrot
+Write a function, closestCarrot, that takes in a grid, a starting row, and a starting column. In the grid, 'X's are walls, 'O's are open spaces, and 'C's are carrots. The function should return a number representing the length of the shortest path from the starting position to a carrot. You may move up, down, left, or right, but cannot pass through walls (X). If there is no possible path to a carrot, then return -1.
+*/
+
+const closestCarrot = (grid, startRow, startCol) => {
+  const visited = new Set(createPos(startRow, startCol));
+  const q = [[startRow, startCol, 0]];
+  while (q.length) {
+    const [r, c, dist] = q.shift();
+    if (grid[r][c] === 'C') return dist;
+    const nextSteps = [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]];
+    for (let step of nextSteps) {
+      const [nextRow, nextCol] = step;
+      const nextPos = createPos(nextRow, nextCol);
+      if (validPos(grid, nextRow, nextCol) && !visited.has(nextPos)) {
+        visited.add(nextPos);
+        q.push([nextRow, nextCol, dist + 1]);
+      };
+    };
+  };
+  return -1;
+};
+
+const createPos = function (r, c) {
+  return r + ',' + c;
+};
+
+const validPos = (grid, r, c) => {
+  const validRow = r >= 0 && r < grid.length;
+  const validCol = c >= 0 && c < grid[0].length;
+  return validRow && validCol && grid[r][c] !== 'X';
+};
+
+test_00:
+const grid = [
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'X', 'O', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['O', 'X', 'C', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['C', 'O', 'O', 'O', 'O'],
+];
+
+closestCarrot(grid, 1, 2); // -> 4
+test_01:
+const grid = [
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'X', 'O', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['O', 'X', 'C', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['C', 'O', 'O', 'O', 'O'],
+];
+
+closestCarrot(grid, 0, 0); // -> 5
+test_02:
+const grid = [
+  ['O', 'O', 'X', 'X', 'X'],
+  ['O', 'X', 'X', 'X', 'C'],
+  ['O', 'X', 'O', 'X', 'X'],
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'X', 'X', 'X', 'X'],
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'C', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O'],
+];
+
+closestCarrot(grid, 3, 4); // -> 9
+test_03:
+const grid = [
+  ['O', 'O', 'X', 'O', 'O'],
+  ['O', 'X', 'X', 'X', 'O'],
+  ['O', 'X', 'C', 'C', 'O'],
+];
+
+closestCarrot(grid, 1, 4); // -> 2
+test_04:
+const grid = [
+  ['O', 'O', 'X', 'O', 'O'],
+  ['O', 'X', 'X', 'X', 'O'],
+  ['O', 'X', 'C', 'C', 'O'],
+];
+
+closestCarrot(grid, 2, 0); // -> -1
+test_05:
+const grid = [
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X'],
+  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'C'],
+];
+
+closestCarrot(grid, 0, 0); // -> -1
